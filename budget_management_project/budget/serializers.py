ae6a3ee39ba32963d2be9d34d9ac8bbab03939d8
minnie-kang <rha0852@gmail.com>
2025-02-10 21:31:59 +0900

    :sparkles:feat: 예산 serializer, urls 설정

---
+++ budget_management_project/budget/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from budget.models import Budget
+from budget_management_project.expense.models import Category
+
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+        
+
+class BudgetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Budget
+        fields = ['id', 'user', 'category', 'budget_money', 'budget_date', 'recommended_amount', 'risk_rate', 'created_at', 'updated_at']
