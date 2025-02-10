c793d7e554a704b5e0a672ce24e7cde28b9f4c9f
wriml92 <wriml92@knou.ac.kr>
2025-02-11 01:23:19 +0900

    :sparkles:feat: 지출 models의 index 추가

--- budget_management_project/expense/models.py
+++ budget_management_project/expense/models.py
@@ -6,7 +6,6 @@ User = get_user_model()
 
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
-    type = models.CharField(max_length=50, null=True, blank=True)
     description = models.CharField(max_length=50, null=True, blank=True)
 
     class Meta:
@@ -17,16 +16,19 @@ class Category(models.Model):
 
 class Expense(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='expenses')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     expense_money = models.PositiveIntegerField(default=0)
-    expense_date = models.DateField()
+    expense_date = models.DateField(db_index=True)
     memo = models.TextField(null=True, blank=True) 
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = "expense"
         ordering = ['-expense_date', '-created_at']
+        indexes = [
+            models.Index(fields=['-expense_date', '-created_at'], name='expense_date_created_idx'),
+        ]
 
     def __str__(self):
         return f"{self.user.username}의 지출: {self.expense_money}원 ({self.expense_date})"
