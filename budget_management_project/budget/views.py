d6bac632ac3aaeb12f1b9a4475a2391906ce9c35
minnie-kang <rha0852@gmail.com>
2025-02-10 17:12:16 +0900

    :bug:fix: 로그인한 사용자만 접근 가능하게 파라미터 수정

--- budget_management_project/budget/views.py
+++ budget_management_project/budget/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render  
 from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -11,7 +11,7 @@ from .serializers import CategorySerializer
 #카테고리 관관APIView
 class CategoryView(APIView):
     # 로그인한 사용자만 접근 가능
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
     
     def get(self, request):
         # 모든 카테고리 조회
