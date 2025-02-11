9068aada20fe2a735deb968fbae5fd1e26101f71
KevinKim384 <wopal7890@gmail.com>
2025-02-11 17:01:57 +0900

    Co-authored-by: minnie <rha0852@gmail.com> Co-authored-by: Coated-jinee <coated.jinee@gmail.com> Co-authored-by: Jae Hyuk, Jung <wriml92@knou.ac.kr> Co-authored-by: 사재혁 <wogur981208@gmail.com>

--- budget_management_project/expense/models.py
+++ budget_management_project/expense/models.py
@@ -1,24 +1,3 @@
 from django.db import models
-from django.contrib.auth import get_user_model
 
-User = get_user_model()
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-    description = models.CharField(max_length=50, null=True, blank=True)
-
-    class Meta:
-        db_table = "category"
-
-class Expense(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    expense_money = models.PositiveIntegerField(default=0)
-    expense_date = models.DateField(auto_now_add=True) 
-    memo = models.TextField(null=True, blank=True) 
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True) 
-
-    class Meta:
-        db_table = "expense"
+# Create your models here.
