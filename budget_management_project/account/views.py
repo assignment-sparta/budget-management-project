f3243785888765189969bde3f5cbf872af7b1ae6
jinee <coated.jinee@gmail.com>
2025-02-11 13:42:19 +0900

    :bug:fix: RegisterView에 가입 액세스를 위한 AllowAny 권한 추가

--- budget_management_project/account/views.py
+++ budget_management_project/account/views.py
@@ -1,10 +1,12 @@
 from rest_framework import generics
+from rest_framework.permissions import AllowAny
 from .serializers import RegisterSerializer
 
 from django.contrib.auth import get_user_model
 
-User = get_user_model
+User = get_user_model()
 
 class RegisterView(generics.CreateAPIView):
+    permission_classes = [AllowAny]
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
