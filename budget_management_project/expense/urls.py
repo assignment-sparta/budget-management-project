d7fd563e9c379eab1542426def849a581fb82756
wriml92 <wriml92@knou.ac.kr>
2025-02-11 17:13:09 +0900

    :sparkles:feat: int:expense_id에서 int:pk로 코드를

--- budget_management_project/expense/urls.py
+++ budget_management_project/expense/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ExpenseCreateView, ExpenseUpdateView
+
+from expense.views import ExpenseCreateView, ExpenseUpdateView
 
 app_name = "expense"
 
 urlpatterns = [
     path('api/v1/expenses', ExpenseCreateView.as_view(), name='expense-create'),
-    path('api/v1/expenses/<int:expense_id>', ExpenseUpdateView.as_view(), name='expense-update'),
+    path('api/v1/expenses/<int:pk>', ExpenseUpdateView.as_view(), name='expense-update'),
 ]
