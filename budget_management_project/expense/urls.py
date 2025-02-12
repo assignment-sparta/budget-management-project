a57125230f8e483cf1771b18b3dd30b295a8b305
wriml92 <wriml92@knou.ac.kr>
2025-02-12 21:38:23 +0900

    :sparkles:feat: import경로 절대경로 변경 및 나머지 기능들에 대한 url 추가

--- budget_management_project/expense/urls.py
+++ budget_management_project/expense/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from expense.views import ExpenseCreateView, ExpenseUpdateView
-
-app_name = "expense"
+from budget_management_project.expense.views import (
+    ExpenseCreateView, ExpenseUpdateView, ExpenseDeleteView, 
+    ExpenseListView, ExpenseDetailView, ExpenseExcludeView
+)
 
 urlpatterns = [
-    path('api/v1/expenses', ExpenseCreateView.as_view(), name='expense-create'),
-    path('api/v1/expenses/<int:expense_id>', ExpenseUpdateView.as_view(), name='expense-update'),
+    path('expenses', ExpenseCreateView.as_view(), name='expense-create'),
+    path('expenses/<int:expense_id>', ExpenseUpdateView.as_view(), name='expense-update'),
+    path('expenses/<int:expense_id>', ExpenseDeleteView.as_view(), name='expense-delete'),
+    path('expenses', ExpenseListView.as_view(), name='expense-list'),
+    path('expenses/<int:expense_id>', ExpenseDetailView.as_view(), name='expense-detail'),
+    path('expenses/<int:expense_id>/exclude', ExpenseExcludeView.as_view(), name='expense-exclude'),
 ]
