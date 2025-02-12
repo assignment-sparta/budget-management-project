1bf71639b37e7788deb3bd96e5257bcf0743c7b6
minnie-kang <rha0852@gmail.com>
2025-02-12 22:56:19 +0900

    :hammer:refact: url패턴 수정

--- config/root_urls.py
+++ config/root_urls.py
@@ -13,10 +13,12 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/", include("budget_management_project.account.urls")),
-    path("api/v1/", include("budget_management_project.budget.urls")),
-    path("api/v1/", include("budget_management_project.expense.urls")), 
-]
+    path("api/v1/", include([
+        "budget_management_project.account.urls",
+        "budget_management_project.budget.urls",
+        "budget_management_project.expense.urls",
+    ])),
+    ]
 
 
 
