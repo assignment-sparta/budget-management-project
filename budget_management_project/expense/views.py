70dd673ec5221f577aa0be867e37de838e98622b
wriml92 <wriml92@knou.ac.kr>
2025-02-13 23:45:21 +0900

    :bug:fix: 공통된 부분은 BaseExpenseView으로 통일

--- budget_management_project/expense/views.py
+++ budget_management_project/expense/views.py
@@ -2,11 +2,10 @@ from django.db import transaction
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated
 
-from budget_management_project.expense.models import Category
-from budget_management_project.expense.serializers import CategorySerializer
-from budget_management_project.expense.serializers import ExpenseSerializer
+from budget_management_project.expense.models import Category, Expense
+from budget_management_project.expense.serializers import CategorySerializer, ExpenseSerializer
 from budget_management_project.expense.permissions import IsExpenseOwner
 
 
@@ -14,7 +13,7 @@ class CategoryView(APIView):
     '''
     카테고리 관련 APIView
     '''
-    permission_classes = [AllowAny] 
+    permission_classes = [IsAuthenticated]
     
     def get(self, request):
         categories = Category.objects.all()
@@ -24,9 +23,24 @@ class CategoryView(APIView):
             "data": serializer.data
         }, status=status.HTTP_200_OK)
 
-class ExpenseCreateView(generics.CreateAPIView):
+
+class BaseExpenseView(generics.GenericAPIView):
     serializer_class = ExpenseSerializer
     permission_classes = [IsExpenseOwner]
+    lookup_url_kwarg = 'expense_id'
+
+    def get_queryset(self):
+        return Expense.objects.all()
+
+
+class ExpenseCreateView(BaseExpenseView, generics.ListCreateAPIView):
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({
+            'message': '지출 목록 조회 성공',
+            'data': serializer.data
+        }, status=status.HTTP_200_OK)
 
     @transaction.atomic
     def create(self, request, *args, **kwargs):
@@ -39,6 +53,41 @@ class ExpenseCreateView(generics.CreateAPIView):
         }, status=status.HTTP_201_CREATED)
 
 
-class ExpenseUpdateView(generics.UpdateAPIView):
-    serializer_class = ExpenseSerializer
-    permission_classes = [IsExpenseOwner]
+class ExpenseDetailView(BaseExpenseView, generics.RetrieveUpdateDestroyAPIView):
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response({
+            'message': '지출 상세 조회 성공',
+            'data': serializer.data
+        }, status=status.HTTP_200_OK)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({
+            'message': '지출이 성공적으로 수정되었습니다.',
+            'data': serializer.data
+        }, status=status.HTTP_200_OK)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.delete()
+        return Response({
+            'message': '지출이 성공적으로 삭제되었습니다.'
+        }, status=status.HTTP_204_NO_CONTENT)
+
+
+class ExpenseExcludeView(BaseExpenseView, generics.UpdateAPIView):
+    def patch(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.is_excluded = not instance.is_excluded
+        instance.save()
+        
+        message = '지출이 합계에서 제외되었습니다.' if instance.is_excluded else '지출이 합계에 포함되었습니다.'
+        return Response({
+            'message': message,
+            'data': self.get_serializer(instance).data
+        }, status=status.HTTP_200_OK)
