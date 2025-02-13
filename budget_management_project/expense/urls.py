fde762391ce3f1f1e43f22d37dc37e0766dd351b
wriml92 <wriml92@knou.ac.kr>
2025-02-13 23:40:02 +0900

    :bug:fix: 불필요한 url 패턴 제거

--- budget_management_project/expense/urls.py
+++ budget_management_project/expense/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path
-from budget_management_project.expense import views  # views 모듈 전체를 import
-
+from budget_management_project.expense import views
 
 urlpatterns = [
     path('categories', views.CategoryView.as_view(), name='category'),
     path('expenses', views.ExpenseCreateView.as_view(), name='expense-create'),
-    path('expenses/<int:expense_id>', views.ExpenseUpdateView.as_view(), name='expense-update'),
-    path('expenses/<int:expense_id>', views.ExpenseDeleteView.as_view(), name='expense-delete'),
-    path('expenses', views.ExpenseListView.as_view(), name='expense-list'),
     path('expenses/<int:expense_id>', views.ExpenseDetailView.as_view(), name='expense-detail'),
     path('expenses/<int:expense_id>/exclude', views.ExpenseExcludeView.as_view(), name='expense-exclude'),
 ]
