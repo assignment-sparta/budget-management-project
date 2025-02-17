2a0a3ae4ec58318aa513ee46f6107379add50346
KevinKim384 <wopal7890@gmail.com>
2025-02-17 15:21:47 +0900

    :hammer:refact: manage 파일, 추천 기능, views 파일 수정

--- budget_management_project/budget/recommendation.py
+++ budget_management_project/budget/recommendation.py
@@ -5,30 +5,26 @@ from collections import defaultdict
 from django.db.models import Sum
 from budget_management_project.budget.models import Budget
 from budget_management_project.expense.models import Expense, Category
+import time
 
-def calculate_and_generate_budget_report(user, serializer):
+def calculate_and_generate_budget_report(user):
     today = datetime.date.today()
-    
-    budget_objs = Budget.objects.filter(user=user, budget_date=today)
-    if not budget_objs.exists():
+
+    latest_budget = Budget.objects.filter(user=user, budget_date=today).order_by('-id').first()
+    if not latest_budget:
         return None, "오늘의 예산 설정부터 해주세요!"
-    
-    total_budget = budget_objs.aggregate(total=sum('budget_amount'))['total'] or 0
+
+    total_budget = latest_budget.budget_amount
     month_days = calendar.monthrange(today.year, today.month)[1]
     remaining_days = max(1, month_days - today.day)
 
-    categories = Category.objects.all()
-    category_names = {category.id: category.description for category in categories}
-    
+    category_name = latest_budget.category.description if latest_budget.category else "알 수 없음"
+    daily_category_budget = math.floor(total_budget / remaining_days)
+
     daily_budget_message = (
         f"오늘 사용 가능한 금액은 총 {total_budget // remaining_days:,}원 이며,\n"
         "각 하루 사용 가능 금액:\n"
+        f"- {category_name}: {daily_category_budget:,}원\n"
     )
-    
-    for budget in budget_objs:
-        category_id = budget.category.id
-        category_name = category_names.get(category_id, "알 수 없음")
-        daily_category_budget = math.floor(budget.budget_amount / remaining_days)
-        daily_budget_message += f"- {category_name}: {daily_category_budget:,}원\n"
-    
+
     return None, daily_budget_message
